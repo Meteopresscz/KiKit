066db001a548abae26663f8c0376558a9affd0c2
Josef Gajdusek <atx@atx.name>
2024-10-31 14:00:40 +0100

    Expand gitCommit and gitDate only based on commits that actually change the PCB file

--- kikit/text.py
+++ kikit/text.py
@@ -27,14 +27,47 @@ class Formatter:
 
 def kikitTextVars(board: pcbnew.BOARD, vars: Dict[str, str]={}) -> Dict[str, Any]:
 
-    def gitCommand(cmd) -> Callable[[], str]:
-        def fn() -> str:
-            try:
-                res = subprocess.check_output(cmd)
-                return res.decode("utf-8").strip()
-            except subprocess.CalledProcessError:
-                return "unknown"
-        return fn
+    def command(cmd) -> str:
+        res = subprocess.check_output(cmd)
+        return res.decode("utf-8").strip()
+
+    def gitDescribeBoard() -> str:
+        try:
+            # TODO: Maybe this should check the entire directory?
+            file_path = board.GetFileName()
+            # First get the latest git tag affecting our board file
+            commit_hash = command(
+                ["git", "log", "-1", "--pretty=format:%H", "--", file_path]
+            )
+
+            # We need to generate the dirty flag manually, since
+            # git describe does not support --dirty=-d if a hash is specified
+            has_unstaged_changes = subprocess.call(
+                ["git", "diff", "--quiet", "--", file_path]
+            )
+            has_staged_changes = subprocess.call(
+                ["git", "diff", "--cached", "--quiet", "--", file_path]
+            )
+            is_dirty = has_unstaged_changes or has_staged_changes
+
+            # Next, use describe on this commit has to generate the resulting string
+            described_hash = command(["git", "describe", "--always", commit_hash])
+
+            if is_dirty:
+                described_hash += "-d"
+
+            return described_hash
+        except subprocess.CalledProcessError:
+            return "unknown"
+
+    def gitDateBoard() -> str:
+        try:
+            file_path = board.GetFileName()
+            return command(
+                ["git", "log", "-1", "--pretty=format:%cs", "--", file_path]
+            )
+        except subprocess.CalledProcessError:
+            return "unknown"
 
     availableVars: Dict[str, Formatter] = {
         "date": Formatter(lambda: dt.datetime.today().strftime("%Y-%m-%d"), vars),
@@ -49,8 +82,8 @@ def kikitTextVars(board: pcbnew.BOARD, vars: Dict[str, str]={}) -> Dict[str, Any
         "boardDate": Formatter(lambda: board.GetTitleBlock().GetDate(), vars),
         "boardRevision": Formatter(lambda: board.GetTitleBlock().GetRevision(), vars),
         "boardCompany": Formatter(lambda: board.GetTitleBlock().GetCompany(), vars),
-        "gitCommit": Formatter(gitCommand(["git", "describe", "--long", "--always", "--dirty=-d"]), vars),
-        "gitDate": Formatter(gitCommand(["git", "show", "-s", "--format=%cs"]), vars),
+        "gitCommit": Formatter(gitDescribeBoard, vars),
+        "gitDate": Formatter(gitDateBoard, vars),
     }
 
     for i in range(10):
