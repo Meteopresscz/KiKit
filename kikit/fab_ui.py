f8d71682895c304a65d0d536024693868301eef2
Josef Gajdusek <atx@atx.name>
2025-04-24 15:47:36 +0200

    feat: Add --remove-footprint flag to kikit fab jlcpcb command

--- kikit/fab_ui.py
+++ kikit/fab_ui.py
@@ -33,6 +33,8 @@ def fabCommand(f):
     help="Which variant to use for the assembly files")
 @click.option("--correctionpatterns", type=click.Path(dir_okay=False))
 @click.option("--missingError/--missingWarn", help="If a non-ignored component misses LCSC field, fail")
+@click.option("--remove-footprint", multiple=True, type=str, default=[],
+    help="Footprint ID (Lib:Footprint) to remove before generating Gerbers. Can be specified multiple times.")
 @click.option("--skip-missing/--no-skip-missing", is_flag=True, help="Skip components with missing or empty LCSC field", default=True)
 def jlcpcb(**kwargs):
     """
